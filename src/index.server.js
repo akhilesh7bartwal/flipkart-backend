48a49822e34990fe42c1e2cea1ce32934cbebbc2
akhilesh7bartwal <akhileshbartwal61@gmail.com>
2023-02-20 23:16:09 +0530

    coming from laptop

--- src/index.server.js
+++ src/index.server.js
@@ -2,8 +2,22 @@ const express = require('express');
 const env = require('dotenv');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
+//enviorment vaiables or you can say constants
 env.config();
+
+//mongodb connection
+//mongodb+srv://akhilesh:<password>@cluster0.ma5a9.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(`mongodb+srv://akhilesh:${process.env.MONGO_DB_PASSWORD}@cluster0.ma5a9.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+).then(()=>{
+    console.log("database connected.");
+});
+
 
 app.use(bodyParser());
 
@@ -22,4 +36,3 @@ app.post('/data',(req,res,next) =>{
 app.listen(process.env.PORT, ()=>{
     console.log(`Server in running on port ${process.env.PORT}`);
 })
-//hihihihih
